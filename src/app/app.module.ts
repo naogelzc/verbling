567f20c5bf6fbe1c02d5e1e82752595490e2ff41
naogelzc <1556797732@qq.com>
2018-12-17 13:04:05 +1300

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { AdComponent } from './ad/ad.component';
 import { ScrollComponent } from './scroll/scroll.component';
+import { SelectComponent } from './select/select.component';
+import { EnterpriseComponent } from './enterprise/enterprise.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,11 @@ import { ScrollComponent } from './scroll/scroll.component';
     NavBarComponent,
     HeaderComponent,
     AdComponent,
-    ScrollComponent
+    ScrollComponent,
+    SelectComponent,
+    EnterpriseComponent,
+    TestimonialsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
